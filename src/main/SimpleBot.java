ff3d1905e381758ab18575e02c3e8b441fcad80d
Baggykiin <jhgeluk@gmail.com>
2013-02-06 20:55:40 +0100

    Fixed some bugs with the -get command

--- src/main/SimpleBot.java
+++ src/main/SimpleBot.java
@@ -18,7 +18,7 @@ public class SimpleBot extends PircBot{
 	
 	// The current version of the bot. Only increment this each time there is a release.
 	// Convention: (milestone).(major)[.(minor).[(revision/bugfix)]]
-	public static final String version = "1.7";
+	public static final String version = "1.7.0.2";
 	
 	// More debug output?
 	private static final boolean verbose = false;
