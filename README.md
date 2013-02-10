8b41ace63b321d982b6b54e455593c484cff0f34
Baggykiin <jhgeluk@gmail.com>
2013-02-11 00:28:00 +0100

    Merge Dev into Master

--- README.md
+++
@@ -1,6 +0,0 @@
-BaggyBot
-========
-
-A PircBot-based IRC bot that collects general statistics about the channel it's in and saves them to a MySQL database.
-
-This branch contains the latest stable version.
---
+++ src/main/Logger.java
@@ -0,0 +1,63 @@
+package main;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+
+public class Logger {
+	private static Logger instance;
+	
+	private File logFile;
+	private Writer fileWriter;
+	private BufferedWriter writer;
+	
+	public static Logger getInstance(){
+		if(instance == null) instance = new Logger();
+		return instance;
+	}
+	public Logger(){
+		try {
+			logFile = new File("BaggyBot.log");
+			logFile.createNewFile();
+			fileWriter = new FileWriter(logFile);
+			writer = new BufferedWriter(fileWriter);
+		} catch (IOException e) {
+			e.printStackTrace();
+			BaggyBot.instance.queueMessage("[WARNING] Failed to open the log file. Debug output will not be available.");
+			
+		}
+	}
+	public boolean flush(){
+		boolean success = true;
+		try {
+			writer.flush();
+		} catch (IOException e) {
+			BaggyBot.instance.addException(e);
+			e.printStackTrace();
+			success = false;
+		}
+		return success;
+	}
+	public static void log(String line){
+		if(instance == null) instance = new Logger();
+		instance.write(line);
+	}
+	public void write(String line){
+		try {
+			writer.write(line + "\r\n");
+		} catch (IOException e) {
+			BaggyBot.instance.addException(e);
+			e.printStackTrace();
+		}
+	}
+	public void close(){
+		try {
+			writer.close();
+		} catch (IOException e) {
+			BaggyBot.instance.addException(e);
+			e.printStackTrace();
+		}
+	}
+}
